cdfbedeb06e5a0b3e1367eb59d17cd3cbc69e1dd
Eng <england.thanarat@gmail.com>
2023-05-12 11:23:26 +0700

    Edit main routing

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import App from './App.jsx'
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import Recruitment from './Recruitment.jsx'
 import Profile from './Profile.jsx'
-import Detail from './Components/Detail.jsx'
 import { MantineProvider, Grid, } from '@mantine/core';
 import { ModalsProvider } from '@mantine/modals';
 
@@ -19,7 +18,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
             <Route path='/' element={<App />} />
             <Route path='/recruitment' element={<Recruitment />} />
             <Route path='/profile' element={<Profile />} />
-            <Route path='/detail' element={<Detail />} />
           </Routes>
         </ModalsProvider>
       </BrowserRouter>
